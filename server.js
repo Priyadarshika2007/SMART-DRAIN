5f7b29015adf1fba705e88aef7c5acaec93f7271
Priyadarshika G K <priyadarshika1129@gmail.com>
2026-04-08 19:56:51 +0530

    fixed mobile preflight cors issue

--- server.js
+++ server.js
@@ -48,8 +48,10 @@ app.use(secureHeaders);
 app.use(express.json());
 app.use(cors({
   origin: '*',
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
+app.options('*', cors());
 app.use('/api', apiRateLimiter);
 
 // Debug middleware to confirm routing path for every request
