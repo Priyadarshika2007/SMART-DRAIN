361b5934aa72365fd535ee5b2dde16a1e83e15ab
Priyadarshika G K <priyadarshika1129@gmail.com>
2026-04-08 20:01:43 +0530

    fix crash remove options cors

--- server.js
+++ server.js
@@ -48,10 +48,8 @@ app.use(secureHeaders);
 app.use(express.json());
 app.use(cors({
   origin: '*',
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
 }));
-app.options('*', cors());
 app.use('/api', apiRateLimiter);
 
 // Debug middleware to confirm routing path for every request
